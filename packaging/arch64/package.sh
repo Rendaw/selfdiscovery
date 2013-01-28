63deee63fe313beb378cea4034e106d31b11b787
Rendaw <rendaw@zarbosoft.com>
2013-01-28 19:26:36 +0900

    Updated packaging.

--- packaging/arch64/package.sh
+++ packaging/arch64/package.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-if [ ! -d ../../variant-release ]
-then
-	echo You must have a release build in variant directory variant-release/ for this to work. 1>&2
-	exit 1
-fi
+. ../include
 mkdir -p src
 cp ../../variant-release/build/selfdiscovery src
-cp ../../LICENSE.txt src
+cp ../../license.txt src
+cp ../../lualicense.txt src
 makepkg --repackage --force
 
---
+++ packaging/ubuntu12/package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+. ../include
+mkdir -p selfdiscovery/usr/bin
+cp ../../variant-release/app/build/selfdiscovery selfdiscovery/usr/bin
+mkdir -p selfdiscovery/usr/share/doc/selfdiscovery
+cp ../../license.txt selfdiscovery/usr/share/doc/selfdiscovery
+cp ../../lualicense.txt selfdiscovery/usr/share/doc/selfdiscovery
+dpkg --build selfdiscovery .
+
